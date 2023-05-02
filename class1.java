bbf8f49b37a80b7d77e98cac939e4e845e996e01
mademaditya3007 <mademaditya3007@gmail.com>
2023-05-02 19:47:25 +0530

    update 1.0.01

--- class1.java
+++ class1.java
@@ -7,7 +7,7 @@ public class class1 {
     public static void main(String[] args){
 
         person p1=new person();
-        p1.name="amazon";
+        
     p1.roll=34;
     System.out.println(p1.name+"    "+p1.roll);
 }
--- class12.java
+++ class12.java
@@ -11,9 +11,7 @@ class person{
 }
 public class class12 {
     public static void main(String[] args){
-        person k1=new person();
-        Scanner k=new Scanner(System.in);
-        int a=k.nextInt();
+        
         int b=k.nextInt();
         System.out.println(k1.sum(a,b));
 
